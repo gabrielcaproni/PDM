6ed5e73ed1b72280ecad72e8834ed9e11411cf29
gabrielcaproni <gabrielcaproni09@gmail.com>
2024-03-26 08:59:02 -0300

    Atualizando projeto Android Studio,implementação de novos botões e códigos.

--- AndroidStudio2024/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
+++ AndroidStudio2024/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
@@ -3,6 +3,9 @@ package br.edu.ifsuldeminas.mch.meuprimeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Acessar o botão
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+
+        //Chamar o OnClickListener
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast =Toast.makeText(view.getContext(), R.string.login_succesfull, Toast.LENGTH_LONG);
+
+                toast.show();
+            }
+        });
+
+        buttonLogin.setOnClickListener(view -> {
+            Toast toast =Toast.makeText(view.getContext(), R.string.login_succesfull, Toast.LENGTH_LONG);
+
+            toast.show();
+        });
+
+
     }
 }
