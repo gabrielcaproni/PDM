dfffc390cdd7840ac25ac41577c035ece2d9a5a4
gabrielcaproni <gabrielcaproni09@gmail.com>
2024-04-02 09:00:50 -0300

    Atualizado dia 02-04-2024

--- AndroidStudio2024/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
+++ AndroidStudio2024/MeuPrimeiroApp/app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
@@ -3,6 +3,7 @@ package br.edu.ifsuldeminas.mch.meuprimeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,23 +11,34 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonLogin;
+
+    private EditText userName;
+
+    private EditText userPW;
+
+    private static final String USER = "Gabriel";
+    private static final String PW = "123";
+
+    private static final String TAG = "login_main_activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonLogin = findViewById(R.id.buttonLogin);
-        EditText userName = findViewById(R.id.editTextUser);
-        EditText userPW = findViewById(R.id.editTextNumberPassword);
-
-        //Recuperando login e senha como string
-        String userNameStr = userName.getText().toString();
-        String userPWStr = userPW.getText().toString();
+        buttonLogin = findViewById(R.id.buttonLogin);
+        userName = findViewById(R.id.editTextUser);
+        userPW = findViewById(R.id.editTextNumberPassword);
 
         buttonLogin.setOnClickListener(view ->{
+
+            String userNameStr = userName.getText().toString();
+            String userPWStr = userPW.getText().toString();
+
             if(userNameStr.equals("")){
                 Toast toast = Toast.makeText(view.getContext(),
-                        R.string.login_user_name_empty, Toast.LENGTH_LONG);
+                        R.string.login_user_name_empty, Toast.LENGTH_SHORT);
 
                 toast.show();
                 return;
@@ -34,12 +46,28 @@ public class MainActivity extends AppCompatActivity {
 
             if(userPWStr.equals("")){
                 Toast toast = Toast.makeText(view.getContext(),
-                        R.string.login_user_pw_empty, Toast.LENGTH_LONG);
+                        R.string.login_user_pw_empty, Toast.LENGTH_SHORT);
 
                 toast.show();
                 return;
             }
+
+            if(userNameStr.equals(USER) && userPWStr.equals(PW)){
+                Toast toast = Toast.makeText(view.getContext(),
+                        R.string.login_succesfull, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            else{
+                Toast toast = Toast.makeText(view.getContext(),
+                        R.string.login_user_wrong_user_name_or_pw, Toast.LENGTH_SHORT);
+                toast.show();
+
+                Log.w(TAG, "Login e senha incorretos");
+            }
         });
+
+        Log.i(TAG, "O metodo onCreat executou sem erros");
     }
 
         public void forgotPW (View view) {
