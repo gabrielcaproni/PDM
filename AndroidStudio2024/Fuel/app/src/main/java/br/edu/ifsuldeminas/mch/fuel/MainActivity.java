e48393567561c58ff4b450ce82d72ec663b5647a
gabrielcaproni <gabrielcaproni09@gmail.com>
2024-07-23 08:58:57 -0300

    Aplicativo FUEL atualizado.

--- AndroidStudio2024/Fuel/app/src/main/java/br/edu/ifsuldeminas/mch/fuel/MainActivity.java
+++ AndroidStudio2024/Fuel/app/src/main/java/br/edu/ifsuldeminas/mch/fuel/MainActivity.java
@@ -1,10 +1,17 @@
 package br.edu.ifsuldeminas.mch.fuel;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,11 +28,51 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
     private String tip;
     private Double etanolPrice, gasPrice;
+    private SharedPreferences preferences;
+    private static final String PREFS_KEY = "br.edu.ifsuldeminas.mch.fuel.prefs";
+    private static final String GAS_KEY = "preco_gas";
+    private static final String ETANOL_KEY = "preco_etanol";
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        String gasPrice = TextInputEditGas.getText().toString();
+        if (!gasPrice.equals("")){
+            editor.putString(GAS_KEY, gasPrice);
+        }
+
+        String etanolPrice = TextInputEditEtanol.getText().toString();
+
+        if (!etanolPrice.equals("")){
+            editor.putString(ETANOL_KEY, etanolPrice);
+        }
+
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (preferences.contains(GAS_KEY)){
+            String gasPrice = preferences.getString(GAS_KEY, "");
+            TextInputEditGas.setText((gasPrice));
+        }
+
+        if (preferences.contains(ETANOL_KEY)){
+            String etanolPrice = preferences.getString(ETANOL_KEY, "");
+            TextInputEditEtanol.setText((etanolPrice));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences(PREFS_KEY, MODE_PRIVATE);
 
          TextInputEditEtanol = findViewById(R.id.textInputEditTextEtanol);
          TextInputEditGas = findViewById(R.id.textInputEditTextGas);
@@ -66,6 +113,42 @@ public class MainActivity extends AppCompatActivity {
              textViewResult.setVisibility(TextView.VISIBLE);
              imageViewShare.setVisibility(ImageView.VISIBLE);
          });
+
+         imageViewShare.setOnClickListener(view -> {
+             AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+             builder.setTitle("Preço de qual posto?");
+
+             LayoutInflater inflater = getLayoutInflater();
+             View layoutDialogView = inflater.inflate(R.layout.alert_dialog_gas_station_view, null);
+             builder.setView(layoutDialogView);
+
+             builder.setNegativeButton("Cancelar", null);
+             builder.setPositiveButton("Enviar", (dialogLayout, button) -> {
+                 EditText editText = layoutDialogView.findViewById(R.id.editTextAlertDialogId);
+                 String posto = editText.getText().toString();
+
+                 if (posto.equals("")){
+                     Toast toast = Toast.makeText(this, "Nome do posto não pode ser vazio!", Toast.LENGTH_SHORT);
+                     toast.show();
+                     return;
+                 }
+
+                 String message = String.format("Preços do posto '%s'. Gasolina: %.2f, Etanol: %.2f. %s. %.2f %s",
+                         posto, gasPrice, etanolPrice, tip, etanolPrice/gasPrice*100, "%");
+
+                 Intent sendIntent = new Intent();
+                 sendIntent.setAction(Intent.ACTION_SEND);
+                 sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+                 sendIntent.setType("text/plain");
+
+                 Intent shareintent = Intent.createChooser(sendIntent, "");
+                 startActivity(shareintent);
+             });
+
+             builder.create().show();
+         });
+
     }
 
     @Override
